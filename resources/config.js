07a94e5ed83fd21b3cd4eda72ce015f10a9da457
mrkoala30 <acasas16@gmail.com>
2017-02-24 11:48:41 +0100

    Update config.js

--- resources/config.js
+++ resources/config.js
@@ -2,7 +2,7 @@ var config = {};
     config.url = {};
     config.transmision = {};
 
-config.url.dir = 'http://adchome.duckdns.org:3000';
+config.url.dir = 'https://hellofinder.herokuapp.com';
 config.transmision.user = 'osmc';
 config.transmision.pass = '240711';
 
